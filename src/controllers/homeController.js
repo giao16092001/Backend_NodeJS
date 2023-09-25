4091b205e6c2e79e63a0fd74f587e32660ebc9c7
giao16092001 <giaopham16092001@gmail.com>
2023-09-25 11:54:47 +0700

    add database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,18 @@
+const connection = require('../config/database')
 // router.Method("route", handler);
 const getHomepage = (req, res) => {
-  res.send("Hello");
+  let users = [];
+
+  connection.query(
+    'SELECT * FROM Users u',
+    function(err, results, fields) {
+      users = results;
+      console.log(">>>results home page= ",results);
+      console.log(">>check users: ",users);
+      res.send(JSON.stringify(users));
+    }
+  );
+  
 };
 const getabc = (req, res) => {
   res.send("Hello");
--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express"); // common js
 const path = require("path"); //common js
 const configViewEngine = require("./config/viewEngine");
+const connection = require("./config/database")
 // import express from 'express'; //es modules
 // console.log(">>>check env: ",process.env)
 const webRoutes = require("./routes/web");
 const app = express(); // app express
 const port = process.env.PORT || 8888; //port => hardcode
 const hostname = process.env.HOST_NAME;
+
 
 //config template engine
 configViewEngine(app);
@@ -17,3 +19,6 @@ app.use("/", webRoutes);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//test connection
+
